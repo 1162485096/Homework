53ce03c4cee1a89bc1fd29dc53e4f4c54c68ca37
1162485096 <37677728+1162485096@users.noreply.github.com>
2018-11-28 20:49:06 +0800

    Update main.py

--- hw04/main.py
+++ hw04/main.py
@@ -1,15 +1,15 @@
 import tornado.web
 import tornado.ioloop
 class aa(tornado.web.RequestHandler):
-    def get(self,x):
+    def get(self,a):
     
-        x=int(x) if x is not None else 9
+        a=int(a) if a is not None else 9
         html = '''
         <html>
         <body>
         <table>
         '''
-        for i in range(1, x+1):
+        for i in range(1, a+1):
             html +='<TR>'
             for j in range(1, i+1):
                 html += '<TD>%d*%d=%d</TD>' % (j,i,i*j)
